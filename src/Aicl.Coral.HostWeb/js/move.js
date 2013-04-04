96bf81e1594f873be236f47e63005ed938291bc2
angel colmenares <angel.ignacio.colmenares@gmail.com>
2013-04-03 23:29:30 -0500

    panels ...

---
+++ src/Aicl.Coral.HostWeb/js/move.js
@@ -0,0 +1,63 @@
+//http://stackoverflow.com/questions/561844/how-to-move-div-with-the-mouse-using-jquery
+//$("#titleBar").draggit("#whatToDrag");
+function (el) {
+    var thisdiv = this;
+    var thistarget = $(el);
+    var relX;
+    var relY;
+    var targetw = thistarget.width();
+    var targeth = thistarget.height();
+    var docw;
+    var doch; var ismousedown=false;
+
+    thistarget.css('position','absolute');
+    thistarget.css('width', targetw);
+    thistarget.css('height', targeth);
+
+
+    thisdiv.bind('mousedown', function(e){
+        var pos = $(el).offset();
+        var srcX = pos.left;
+        var srcY = pos.top;
+
+        docw = $('body').width();
+        doch = $('body').height();
+
+        relX = e.pageX - srcX;
+        relY = e.pageY - srcY;
+
+        ismousedown = true;
+    });
+
+    $(document).bind('mousemove',function(e){ 
+        if(ismousedown)
+        {
+            targetw = thistarget.width();
+            targeth = thistarget.height();
+
+            var maxX = docw - targetw - 10;
+            var maxY = doch - targeth - 10;
+
+            var mouseX = e.pageX;
+            var mouseY = e.pageY;
+
+            var diffX = mouseX - relX;
+            var diffY = mouseY - relY;
+
+            // check if we are beyond document bounds ...
+            if(diffX < 0)   diffX = 0;
+            if(diffY < 0)   diffY = 0;
+            if(diffX > maxX) diffX = maxX;
+            if(diffY > maxY) diffY = maxY;
+
+            $(el).css('top', (diffY)+'px');
+            $(el).css('left', (diffX)+'px');
+        }
+    });
+
+    $(window).bind('mouseup', function(e){
+        ismousedown = false;
+    });
+
+    return this;
+}
